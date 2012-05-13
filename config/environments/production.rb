2717ff3bf4ea3f0d513d66c7eb6fd8e37c7ee2c8
amardaxini <amardaxini@gmail.com>
2012-05-13 12:31:58 +0530

    set prod log env for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,5 +64,8 @@ AlumniLabs::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+ # FOR HEROKU UNICORN 
+config.logger = Logger.new(STDOUT)
+config.logger.level = Logger.const_get(ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].upcase : 'INFO')
 end
 require 'pry'
